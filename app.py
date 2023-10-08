ab82894199a3c556ca02690adb2367b098e49973
phillip <phillip.lloyd@student.moringaschool.com>
2023-10-08 16:37:21 +0300

    test

--- app.py
+++ app.py
@@ -1,18 +1,57 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from models import db, Product
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-@app.route('/api/post', methods=['POST'])
-def receive_post_request():
-    # Check if the request contains JSON data
-    if request.is_json:
-        data = request.get_json()
-        # You can access the JSON data as a Python dictionary
-        # For example, if the JSON contains a "message" field:
-        message = data.get('message', 'No message provided')
-        return jsonify({'received_message': message})
-    else:
-        return jsonify({'error': 'Invalidi JSON data'}), 400
+CORS(app)
+migrate = Migrate(app, db)
+db.init_app(app)
+api = Api(app)
 
-if __name__ == '__main__':
+class Products(Resource):
+    def get(self):
+        products_dict_list = [d.to_dict() for d in Product.query.all()]
+        response = make_response(
+            jsonify(products_dict_list),
+            200,
+        )
+        return response
+
+    def post(self):
+        products = []
+        for data in request.json:
+            name = data['product_name']
+            quantity = data['product_quantity']
+            price = data['product_price']
+
+            new_product = Product(
+                product_name=name,
+                product_quantity=quantity,
+                product_price=price,
+            )
+
+            db.session.add(new_product)
+            products.append(new_product)
+
+        db.session.commit()
+
+        products_dicts = [products.to_dict() for product in products]
+
+        response = make_response(
+            jsonify(products_dicts),
+            201
+        )
+
+        return response
+
+api.add_resource(Products, '/products')
+
+if __name__ == "__main__":
     app.run(debug=True)
